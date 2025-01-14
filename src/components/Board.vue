4c63c99f708736c93ee34542831000a7dd1ffcb9
AmnesiaBeing <727184139@qq.com>
2025-01-14 22:48:36 +0800

    增加鼠标悬浮效果

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 
-type Note = [
+type Drafts = [
   boolean,
   boolean,
   boolean,
@@ -13,26 +13,37 @@ type Note = [
   boolean
 ];
 
-const grid = computed(() => {
-  let grid: {
-    id: number;
-    class: string;
-    value: string | null;
-    notes: Note;
-  }[][] = [];
-  for (let i = 0; i < 9; i++) {
-    let rows: {
-      id: number;
-      class: string;
-      value: string | null;
-      notes: Note;
-    }[] = [];
-    for (let j = 0; j < 9; j++) {
+type GridRender = {
+  id: number;
+  r: number;
+  c: number;
+  g: number;
+  n: number;
+  cell_class: string;
+  value: string | null;
+  drafts: Drafts;
+};
+
+const hoveredCell = ref<GridRender | null>(null);
+
+const grid_render = computed(() => {
+  let grid: GridRender[][] = [];
+  for (let r = 0; r < 9; r++) {
+    let rows: GridRender[] = [];
+    for (let c = 0; c < 9; c++) {
+      let g = Math.floor(r / 3) * 3 + Math.floor(c / 3);
+      let n = r * 9 + c;
+      let cell_class = "";
+
       rows.push({
-        id: i * 9 + j,
-        class: "",
+        id: r * 9 + c,
+        r,
+        c,
+        g,
+        n,
+        cell_class,
         value: null,
-        notes: [false, false, true, false, false, true, false, false, true],
+        drafts: [false, false, true, false, false, true, false, false, true],
       });
     }
     grid.push(rows);
@@ -40,25 +51,48 @@ const grid = computed(() => {
   return grid;
 });
 
-function onSelect(id: number) {}
+function onSelect(cell: GridRender) {}
+
+function onHover(cell: GridRender) {
+  hoveredCell.value = cell;
+}
+
+function onLeave() {
+  console.log("leave");
+  hoveredCell.value = null;
+}
+
+function isHighlighted(cell: GridRender) {
+  if (hoveredCell.value) {
+    const { r: r1, c: c1, g: g1 } = hoveredCell.value;
+    const { r: r2, c: c2, g: g2 } = cell;
+    return r1 === r2 || c1 === c2 || g1 === g2;
+  }
+}
 </script>
 
 <template>
   <div class="board-container">
-    <div class="board">
-      <div class="board-row" v-for="(row, r) in grid" :key="r">
+    <div class="board" @mouseleave="onLeave">
+      <div class="board-row" v-for="(row, r) in grid_render" :key="r">
         <div
           class="board-col board-cell"
           v-for="cell in row"
           :key="cell.id"
-          :class="cell.class"
-          @click="onSelect(cell.id)"
+          :class="isHighlighted(cell) ? 'cell-highlight' : ''"
+          @click="onSelect(cell)"
+          @mouseover="onHover(cell)"
         >
           <div class="cell-value" v-if="cell.value">
             {{ cell.value }}
           </div>
-          <div class="cell-notes" v-else-if="cell.notes.length > 0">
-            <div class="notes-item" v-for="(note, n) in cell.notes" :key="n">
+          <div class="cell-notes" v-else-if="cell.drafts.length > 0">
+            <div
+              class="notes-item"
+              :class="note ? 'notes-item-highlight' : ''"
+              v-for="(note, n) in cell.drafts"
+              :key="n"
+            >
               {{ note ? n + 1 : " " }}
             </div>
           </div>
